c05f69edf5b6533cf9f43ed73dbc8b16536db573
Bart <bart1991564@hotmail.com>
2022-12-01 15:20:11 -0600

    Commit main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,107 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  function pickRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  function handleIndex(){
+     const next = Math.min(indx + 3, 949);
+     setIndx(next);
+  }
+  const [indx, setIndx] = useState(1);
+  const [pokemon, setPokemon] = useState({
+    pokemonIndex: indx,
+    pokemonData: {},
+    pokemonDescription: "",
+    speciesData: {},
+    evoSprites: [],
+    evoNames: [],
+    moves: [],
+    loading: false
+  });
+  useEffect(() => {
+    const request = `https://pokeapi.co/api/v2/pokemon/${indx}`;
+        fetch(request, {
+            cache: "force-cache"
+        })
+            .then(response => response.json())
+            .then(data => {
+                setPokemon(pokemon => {
+                  return{
+                    ...pokemon,
+                    pokemonData: data,
+                    pokemonIndex: data.id
+                  }
+                });
+                const speciesRequest = data.species.url;
+                return fetch(speciesRequest);
+            })
+            .then(response => response.json())
+            .then(data => {
+                setPokemon(pokemon => {
+                  return{
+                    ...pokemon,
+                    speciesData: data,
+                    description: pickRandom(
+                        data.flavor_text_entries.filter(e => e.language.name === "en").map(e => e.flavor_text)
+                    ),
+                    loading: false
+                  }
+                });
+                const evo_chain = data.evolution_chain.url;
+                fetch(evo_chain)
+                    .then(response => response.json())
+                    .then(data => {
+                        const api = "https://pokeapi.co/api/v2/pokemon/";
+                        const first = data.chain;
+                        let second;
+                        let third;
+                        let evos = [];
+                        if (first) {
+                            const e1 = fetch(`${api}${first.species.name}/`);
+                            evos.push(e1);
+                            second = first.evolves_to[0];
+                        }
+                        if (second) {
+                            const e2 = fetch(`${api}${second.species.name}/`);
+                            third = second.evolves_to[0];
+
+                            evos.push(e2);
+                        }
+                        if (third) {
+                            const e3 = fetch(`${api}${third.species.name}/`);
+                            evos.push(e3);
+                        }
+                        Promise.all(evos)
+                            .then(responses => Promise.all(responses.map(value => value.json())))
+                            .then(dataList => {
+                                const sprites = dataList.map(v => v.sprites.front_default);
+                                const names = dataList.map(n => n.name);
+                                setPokemon(pokemon => {
+                                  return{
+                                    ...pokemon,
+                                     evoSprites: sprites, evoNames: names 
+                                  }
+                                });
+                            });
+                    });
+            });
+  }, [indx])
   return (
+    <>
     <div className="App">
       <header className="App-header">
-          Learn React
+        {
+          pokemon.evoSprites.map(img => (
+            <img src={img}/>
+          ))
+        }
+          
       </header>
     </div>
+    <button onClick={handleIndex}>+</button>
+    <button>-</button>
+    </>
   );
 }
 
